0107bda746c1cda52eea692a9d094dfd11a64e9a
Guilherme Piccoli <guuipiccoli@gmail.com>
2020-03-17 14:22:59 -0300

    table view sections

--- moviedb-objc/ViewController.m
+++ moviedb-objc/ViewController.m
@@ -23,25 +23,52 @@ NSArray *moviesDesc;
     [super viewDidLoad];
     movies= @[@"Filme 1", @"Filme 2", @"Filme 3"];
     moviesDesc= @[@"Filme 1 v moisvdmios msoi os siofn inusnf indfnidf indv insf niussonidsnm so smod mods nisnid s", @"Filme 2 moadsimoavmoisd imo moi omisf mofd oiddmoif dfmoi dmio", @"Filme 3 msaodvomsvmois miod moisf mos msim s nsm sm i mis nisdnisidnsn"];
-    
-    self.navigationItem.title = @"Movies";
-    self.navigationController.navigationBar.prefersLargeTitles = YES;
 }
 
 
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
     MovieCell *cell = (MovieCell *) [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    cell.movieImage.image = [UIImage imageNamed: @"List"];
-    cell.movieRating.text = @"5";
-    cell.movieDescription = [moviesDesc objectAtIndex:indexPath.row];
-    cell.movieTitle = [movies objectAtIndex:indexPath.row];
+    
+    NSString *movieTitle = movies[indexPath.row];
+    NSString *movieDesc = moviesDesc[indexPath.row];
+
+    
+    if (indexPath.section == 0 ) {
+        cell.movieImage.image = [UIImage imageNamed: @"List"];
+        cell.movieRating.text = @"5";
+        cell.movieDescription.text = movieDesc;
+        cell.movieTitle.text = movieTitle;
+    }
+    else {
+        cell.movieImage.image = [UIImage imageNamed: @"List"];
+        cell.movieRating.text = @"0";
+        cell.movieDescription.text = movieDesc;
+        cell.movieTitle.text = movieTitle;
+    }
 
     return cell;
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    if (section == 0) {
+        return 3;
+    }
+    else {
+        return 1;
+    }
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return @"Popular Movies";
+    }
+    else {
+        return @"Now playing";
+    }
+}
 
 @end
